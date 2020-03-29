ca30683e93caf9aadec6dad53b76bfd11744a081
toti65bis <toti65bis@gmail.com>
2020-03-29 19:02:16 -0300

    se agrega features 12

--- configuration/iaac/azure/backend/main.tf
+++ configuration/iaac/azure/backend/main.tf
@@ -1,11 +1,11 @@
 
  provider "azurerm" {
-  version = "~> 1.42"
-  client_id       = var.client_id   # ENVIRONMENT VARIABLE
-  client_secret   = var.client_secret # ENVIRONMENT VARIABLE
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  features {}
+  version = "~> 1.44"
+  #client_id       = var.client_id   # ENVIRONMENT VARIABLE
+  #client_secret   = var.client_secret # ENVIRONMENT VARIABLE
+  #subscription_id = var.subscription_id
+  #tenant_id       = var.tenant_id
+  #features {}
  }
 
  
