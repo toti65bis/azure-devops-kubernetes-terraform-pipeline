ef198577b8e4d5734e0afc88c615a7ebbacd154e
toti65bis <toti65bis@gmail.com>
2020-03-29 19:16:08 -0300

    se agrega features 15

--- configuration/iaac/azure/backend/main.tf
+++ configuration/iaac/azure/backend/main.tf
@@ -1,10 +1,12 @@
 
  provider "azurerm" {
-  version = "~> 1.44"
+  #version = "~> 1.44"
   #client_id       = var.client_id   # ENVIRONMENT VARIABLE
   #client_secret   = var.client_secret # ENVIRONMENT VARIABLE
   #subscription_id = var.subscription_id
   #tenant_id       = var.tenant_id
+  #features {}
+  version         = ">= 2.0"
   features {}
  }
 
